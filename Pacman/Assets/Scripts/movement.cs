a3d6f751dcc1b2eb59cc95bf378ab928946facdd
JesseGabrielGames <44761808+JesseGabrielGames@users.noreply.github.com>
2018-11-04 22:58:30 -0500

    First Commit
    Still having issues with circle collider

---
+++ Pacman/Assets/Scripts/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public float speed = 4.0f;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
